8309469ca5700b96040b84cd380a98a0131c57c1
Huang Xiaolong <hxl65536@gmail.com>
2021-09-29 20:47:20 +0800

    new sync and build scripts

---
+++ scripts/sync_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REMOTE_PATH=/home/huangxl/rdma_train
+
+rsync -a --delete --exclude "*.o" ./ d:/home/huangxl/rdma_train
+
+ssh d "screen -d -m bash -c \"cd $REMOTE_PATH/write_poll; make clean; make LD=aarch64-linux-gnu-gcc \" "
